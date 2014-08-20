de786abdb8e84044a84cc119e24481ec645eb322
alfonsopintos <alfonsopintos@gmail.com>
2014-08-20 15:57:35 -0400

    first rainforest commit

---
+++ app/models/product.rb
@@ -0,0 +1,16 @@
+class Product < ActiveRecord::Base
+	validates :description, :name, presence: true
+	
+	validates :price_in_cents,numericality: {only_integer: true, greater_than: 0} 
+
+	def formatted_price
+		price_in_dollars = price_in_cents.to_f / 100
+		sprintf("%.2f", price_in_dollars)
+	end
+
+	has_many :reviews
+
+	def to_s
+		"#{name} - (#{formatted_price})"
+	end
+end
